68507ad34845b73dacb73ebd2c5902d2e3a1d716
Ogabek Mustafoev <mustafoev0806@gmail.com>
2024-08-20 12:09:43 +0500

    sixth: fixing opengraph

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,71 +1,13 @@
 import {BaseLayout} from "@/components";
-import {logoIconImg, logoShortImg} from "@/constants";
+import {logoShortImg} from "@/constants";
 import {Providers} from "@/providers";
 import {IChildren, IParams} from "@/types";
-import {Metadata} from "next";
-import {NextIntlClientProvider, useMessages} from "next-intl";
+import {NextIntlClientProvider, useMessages, useTranslations} from "next-intl";
 import {Saira_Condensed} from 'next/font/google';
 
 import '@/styles/global.scss';
 
 const saira = Saira_Condensed({subsets: ['latin'], weight: ['300', '400', '500', '600', '700']});
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://vanguard-group.com'),
-  title: {
-    default: "Vanguard - Leading Provider of Mining Equipment",
-    template: "%s | Vanguard",
-  },
-  description: "Welcome to Vanguard. We offer a comprehensive range of high-quality mining equipment tailored to meet the diverse needs of our customers. Explore our services, projects, and partnerships for superior mining solutions.",
-  keywords: "Vanguard, Vanguard, Vanguard, mining equipment, high-quality, reliable, dump trucks, drill rigs, mining solutions",
-  openGraph: {
-    type: "website",
-    siteName: "Vanguard",
-    images: [
-      {
-        url: logoShortImg.src,
-        width: 1200,
-        height: 630,
-        alt: "Vanguard Mining Equipment",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@vanguard-group",
-    creator: "@vanguard-group",
-    images: [
-      {
-        url: logoShortImg.src,
-        width: 1200,
-        height: 630,
-        alt: "Vanguard Mining Equipment",
-      },
-    ],
-  },
-  authors: [
-    {name: "Ogabek Mustafoev", url: "https://mustafoev-ogabek.vercel.app/"},
-    {name: "VANGUARD-TECH-GROUP", url: "https://vanguar-group.com"},
-  ],
-  icons: [
-    {
-      url: logoShortImg.src,
-      type: "image/jpeg",
-      sizes: "1200x630",
-    },
-    {
-      url: logoIconImg.src,
-      type: "image/png",
-      sizes: "32x32",
-    },
-    {
-      url: logoIconImg.src,
-      type: "image/png",
-      sizes: "16x16",
-    },
-  ],
-  robots: "index, follow"
-};
 
 interface IRootLayout extends IChildren {
   params: IParams;
@@ -73,10 +15,31 @@ interface IRootLayout extends IChildren {
 
 export default function RootLayout({children, params: {locale}}: Readonly<IRootLayout>) {
   const messages = useMessages();
+  const t = useTranslations();
 
   return (
     <html lang={locale}>
-
+    <head>
+      <title>{t('seo.title')}</title>
+      <meta name="description" content={t('seo.description')}/>
+      <meta name="keywords" content={t('seo.keywords')}/>
+      <meta property="og:description" content={t('seo.description')}/>
+      <meta property="og:image" content={logoShortImg.src}/>
+      <meta property="og:image:type" content="image/jpeg"/>
+      <meta property="og:image:width" content="1200"/>
+      <meta property="og:image:height" content="630"/>
+      <meta property="og:image:alt" content="Vanguard"/>
+      <meta property="og:url" content="https://vanguar-group.com"/>
+      <meta property="og:type" content="website"/>
+      <meta name="twitter:title" content={t('seo.title')}/>
+      <meta name="twitter:description" content={t('seo.description')}/>
+      <meta name="twitter:image" content={logoShortImg.src}/>
+      <meta property="twitter:image:alt" content="vanguar-group"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <link rel="canonical" href="https://vanguar-group.com"/>
+      <meta name="robots" content="index, follow"/>
+      <meta name="robots" content="max-image-preview:large"/>
+    </head>
     <body className={saira.className}>
     <NextIntlClientProvider locale={locale} messages={messages}>
       <Providers locale={locale}>
