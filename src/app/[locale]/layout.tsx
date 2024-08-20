9c17ddeb89dab206d79a8b4a2e075268926bc7ba
Ogabek Mustafoev <mustafoev0806@gmail.com>
2024-08-20 13:09:15 +0500

    ready

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({children, params: {locale}}: Readonly<IRootL
       <link rel="canonical" href="https://vanguar-group.com"/>
       <meta name="robots" content="index, follow"/>
       <meta name="robots" content="max-image-preview:large"/>
-      <link rel="icon" type="image/svg+xml" sizes="192x192" href={logoIconImg.src}/>
-      <link rel="icon" type="image/svg+xml" sizes="32x32" href={logoIconImg.src}/>
+      <link rel="icon" type="image/png" sizes="192x192" href={logoIconImg.src}/>
+      <link rel="icon" type="image/png" sizes="32x32" href={logoIconImg.src}/>
     </head>
     <body className={saira.className}>
     <NextIntlClientProvider locale={locale} messages={messages}>
